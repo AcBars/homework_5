a53b000b3e3f6b05c9ea95018848ef529d6b1176
Mikhaylov Artem <gits.info20@gmail.com>
2022-06-23 19:49:58 +0300

    task_36 ver.2 working

--- task_36/Program.cs
+++ task_36/Program.cs
@@ -1,8 +1,7 @@
 ﻿/*
-Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов,
+стоящих на нечётных позициях.
 [3, 7, 23, 12] -> 19
-
 [-4, -6, 89, 6] -> 0
 */
 
@@ -10,6 +9,9 @@ using static System.Console;
 Clear();
 WriteLine("Введите длину массива.");
 int size=int.Parse(ReadLine());
+int [] array=GetRandomArray(size);
+WriteLine($"[{String.Join(" ,", array)}]");
+WriteLine($"Сумма элементов массива стоящих на нечётных позициях равна {FidOddArr(array)}");
 
 
 
@@ -25,4 +27,15 @@ int [] GetRandomArray(int size)
         arr[i]=new Random().Next(0,100);
     }
     return arr;
+}
+
+
+int FidOddArr(int [] arr)
+{
+    int sumodd=0;
+    for(int i=1; i<arr.Length;i+=2)
+    {
+        sumodd+=arr[i];
+    }
+    return sumodd;
 }
