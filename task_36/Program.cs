bec99d28036a91e1831c646943cda1295367a9f2
Artem Mikhailob <mobail_2003@bk.ru>
2022-06-23 12:03:07 +0300

    task_36 ver.1 not working

---
+++ task_36/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+*/
+
+using static System.Console;
+Clear();
+WriteLine("Введите длину массива.");
+int size=int.Parse(ReadLine());
+
+
+
+
+
+
+
+int [] GetRandomArray(int size)
+{
+    int [] arr=new int [size];
+    for (int i=0; i<size; i++)
+    {
+        arr[i]=new Random().Next(0,100);
+    }
+    return arr;
+}
